18d5037b0724a326cf9f50f44e7260aaf5d2b6cf
Stephen <stephe.sutjiadi27@gmail.com>
2024-02-12 11:03:24 +0700

    create entities

--- generate/sql/migrate.go
+++ generate/sql/migrate.go
@@ -5,7 +5,7 @@ import (
 
 	// masteroperationentities "after-sales/api/entities/master/operation"
 
-	masterwarehouseentities "after-sales/api/entities/master/warehouse"
+	masterentities "after-sales/api/entities/master"
 	"time"
 
 	"fmt"
@@ -75,8 +75,9 @@ func Migrate() {
 		// // &masteritementities.ItemDetail{},
 		// &masteritementities.DiscountPercent{},
 		// &masterentities.IncentiveGroup{},
+		&masterentities.WarrantyFreeService{},
 
-		&masterwarehouseentities.WarehouseGroup{},
+		// &masterwarehouseentities.WarehouseGroup{},
 		// &masterwarehouseentities.WarehouseMaster{},
 		// &masterwarehouseentities.WarehouseLocation{},
 
