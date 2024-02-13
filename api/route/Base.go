a0ddd466894971f530f15b11f92eaf67bb291462
Enrico Fernando Chai <enrikofernando@gmail.com>
2024-02-13 14:04:51 +0700

    delete comment

--- api/route/Base.go
+++ api/route/Base.go
@@ -174,10 +174,10 @@ func ForecastMasterRouter(
 	forecastMasterController mastercontroller.ForecastMasterController,
 ) *httprouter.Router {
 	router := httprouter.New()
+	router.GET("/forecast-master/", forecastMasterController.GetAllForecastMaster)
 	router.GET("/forecast-master/:forecast_master_id", forecastMasterController.GetForecastMasterById)
 	router.POST("/forecast-master/", forecastMasterController.SaveForecastMaster)
 	router.PATCH("/forecast-master/:forecast_master_id", forecastMasterController.ChangeStatusForecastMaster)
-	router.GET("/forecast-master/", forecastMasterController.GetAllForecastMaster)
 
 	router.PanicHandler = exceptions.ErrorHandler
 
@@ -198,118 +198,3 @@ func adaptHandler(h http.Handler) httprouter.Handle {
 func swaggerHandler() http.HandlerFunc {
 	return httpSwagger.Handler(httpSwagger.URL("/swagger/doc.json"))
 }
-
-// type OperationGroupController interface {
-// 	GetAllOperationGroup(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
-// 	GetAllOperationGroupIsActive(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
-// 	GetOperationGroupByCode(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
-// 	SaveOperationGroup(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
-// 	ChangeStatusOperationGroup(writer http.ResponseWriter, request *http.Request, params httprouter.Params)
-// }
-
-// func CreateHandler(db *gorm.DB, env string, redis *redis.Client) {
-// 	r := gin.New()
-// 	//mtr_operation_group
-// 	operationGroupRepository := masteroperationrepositoryimpl.StartOperationGroupRepositoryImpl(db)
-// 	operationGroupService := masteroperationserviceimpl.StartOperationGroupService(operationGroupRepository)
-// 	//mtr_operation_section
-// 	operationSectionRepository := masteroperationrepositoryimpl.StartOperationSectionRepositoryImpl(db)
-// 	operationSectionService := masteroperationserviceimpl.StartOperationSectionService(operationSectionRepository)
-// 	//mtr_operation_key
-// 	operationKeyRepository := masteroperationrepositoryimpl.StartOperationKeyRepositoryImpl(db)
-// 	operationKeyService := masteroperationserviceimpl.StartOperationKeyService(operationKeyRepository)
-// 	// //mtr_operation_code
-// 	operationCodeRepository := masteroperationrepositoryimpl.StartOperationCodeRepositoryImpl(db)
-// 	operationCodeService := masteroperationserviceimpl.StartOperationCodeService(operationCodeRepository)
-// 	// //mtr_operation_entries
-// 	operationEntriesRepository := masteroperationrepositoryimpl.StartOperationEntriesRepositoryImpl(db)
-// 	operationEntriesService := masteroperationserviceimpl.StartOperationEntriesService(operationEntriesRepository)
-// 	// //mtr_operation_model_mapping
-// 	operationModelMappingRepository := masteroperationrepositoryimpl.StartOperationModelMappingRepositoryImpl(db)
-// 	operationModelMappingService := masteroperationserviceimpl.StartOperationMappingService(operationModelMappingRepository)
-
-// 	//mtr_markup_master
-// 	markupMasterRepository := masteritemrepositoryimpl.StartMarkupMasterRepositoryImpl(db)
-// 	markupMasterService := masteritemserviceimpl.StartMarkupMasterService(markupMasterRepository)
-
-// 	//mtr_uom
-// 	UnitOfMeasurementRepository := masteritemrepositoryimpl.StartUnitOfMeasurementRepositoryImpl(db)
-// 	UnitOfMeasurementService := masteritemserviceimpl.StartUnitOfMeasurementService(UnitOfMeasurementRepository)
-// 	//mtr_item
-// 	itemRepository := masteritemrepositoryimpl.StartItemRepositoryImpl(db, redis)
-// 	itemService := masteritemserviceimpl.StartItemService(itemRepository)
-// 	//mtr_discount
-// 	discountRepository := masterrepositoryimpl.StartDiscountRepositoryImpl(db)
-// 	discountService := masterserviceimpl.StartDiscountService(discountRepository)
-// 	//mtr_incentive_group
-// 	incentiveGroupRepository := masterrepositoryimpl.StartIncentiveGroupImpl(db)
-// 	incentiveGroupService := masterserviceimpl.StartIncentiveGroup(incentiveGroupRepository)
-// 	//mtr_item_class
-// 	itemClassRepository := masteritemrepositoryimpl.StartItemClassRepositoryImpl(db)
-// 	itemClassService := masteritemserviceimpl.StartItemClassService(itemClassRepository)
-// 	//mtr_price_list
-// 	priceListRepository := masteritemrepositoryimpl.StartPriceListRepositoryImpl(db)
-// 	priceListService := masteritemserviceimpl.StartPriceListService(priceListRepository)
-
-// 	//mtr_discount_percent
-// 	discountPercentRepository := masteritemrepositoryimpl.StartDiscountPercentRepositoryImpl(db)
-// 	discountPercentService := masteritemserviceimpl.StartDiscountPercentService(discountPercentRepository)
-
-// 	//mtr_item_level
-// 	itemLevelRepository := masteritemrepositoryimpl.StartItemLevelRepositoryImpl(db)
-// 	itemLevelService := masteritemserviceimpl.StartItemLevelService(itemLevelRepository)
-
-// 	warehouseGroupRepository := masterwarehouserepositoryimpl.OpenWarehouseGroupImpl(db)
-// 	warehouseGroupService := masterwarehouseserviceimpl.OpenWarehouseGroupService(warehouseGroupRepository)
-
-// 	warehouseMasterRepository := masterwarehouserepositoryimpl.OpenWarehouseMasterImpl(db)
-// 	warehouseMasterService := masterwarehouseserviceimpl.OpenWarehouseMasterService(warehouseMasterRepository)
-
-// 	warehouseLocationRepository := masterwarehouserepositoryimpl.OpenWarehouseLocationImpl(db)
-// 	warehouseLocationService := masterwarehouseserviceimpl.OpenWarehouseLocationService(warehouseLocationRepository)
-
-// 	bookingEstimationRepository := transactionworkshoprepositoryimpl.OpenBookingEstimationImpl(db)
-// 	bookingEstimationService := transactionworkshopserviceimpl.OpenBookingEstimationServiceImpl(bookingEstimationRepository)
-
-// 	r.Use(middlewares.SetupCorsMiddleware())
-
-// 	if env != "prod" {
-// 		r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-// 	}
-
-// api := r.Group("/api/aftersales")
-// //master
-// mastercontroller.StartDiscountRoutes(db, api, discountService)
-// mastercontroller.StartIncentiveGroupRoutes(db, api, incentiveGroupService)
-// //operation
-// masteroperationcontroller.StartOperationGroupRoutes(db, api, operationGroupService)
-// masteroperationcontroller.StartOperationSectionRoutes(db, api, operationSectionService)
-// masteroperationcontroller.StartOperationKeyRoutes(db, api, operationKeyService)
-// masteroperationcontroller.StartOperationCodeRoutes(db, api, operationCodeService)
-// masteroperationcontroller.StartOperationEntriesRoutes(db, api, operationEntriesService)
-// masteroperationcontroller.StartOperationModelMappingRoutes(db, api, operationModelMappingService)
-// masteritemcontroller.StartUnitOfMeasurementRoutes(db, api, UnitOfMeasurementService)
-// masteritemcontroller.StartItemRoutes(db, api, itemService)
-// masteritemcontroller.StartItemLevelRoutes(db, api, itemLevelService)
-// masteritemcontroller.StartItemClassRoutes(db, api, itemClassService)
-// masteritemcontroller.StartPriceListRoutes(db, api, priceListService)
-// masteritemcontroller.StartMarkupMasterRoutes(db, api, markupMasterService)
-// masteritemcontroller.StartMarkupRateRoutes(db, api, markupRateService)
-// masteritemcontroller.StartDiscountPercentRoutes(db, api, discountPercentService)
-
-// 	masterwarehousecontroller.OpenWarehouseGroupRoutes(db, api, warehouseGroupService)
-// 	masterwarehousecontroller.OpenWarehouseMasterRoutes(db, api, warehouseMasterService)
-// 	masterwarehousecontroller.OpenWarehouseLocationRoutes(db, api, warehouseLocationService)
-// 	// //transaction
-// 	// transactioncontroller.StartSupplySlipRoutes(db, api.Group("/transaction"), supplySlipService)
-
-// 	transactionworkshopcontroller.OpenBookingEstimationRoutes(db, api, bookingEstimationService)
-
-// 	server := &http.Server{Handler: r}
-// 	l, err := net.Listen("tcp4", fmt.Sprintf(":%v", config.EnvConfigs.Port))
-// 	err = server.Serve(l)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 		return
-// 	}
-// }
