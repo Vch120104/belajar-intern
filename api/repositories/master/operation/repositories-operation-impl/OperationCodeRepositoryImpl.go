5034f7276ce204af423441dabac3c65dbe0744f6
Nats46 <132544024+Nats46@users.noreply.github.com>
2024-04-04 16:00:22 +0700

    change size of int in entities

--- api/repositories/master/operation/repositories-operation-impl/OperationCodeRepositoryImpl.go
+++ api/repositories/master/operation/repositories-operation-impl/OperationCodeRepositoryImpl.go
@@ -1,12 +1,15 @@
 package masteroperationrepositoryimpl
 
 import (
+	masteritementities "after-sales/api/entities/master/item"
 	masteroperationentities "after-sales/api/entities/master/operation"
+	exceptionsss_test "after-sales/api/expectionsss"
 	masteroperationpayloads "after-sales/api/payloads/master/operation"
 	"after-sales/api/payloads/pagination"
 	masteroperationrepository "after-sales/api/repositories/master/operation"
 	"after-sales/api/utils"
 	"log"
+	"net/http"
 
 	"gorm.io/gorm"
 )
@@ -19,21 +22,27 @@ func StartOperationCodeRepositoryImpl(db *gorm.DB) masteroperationrepository.Ope
 	return &OperationCodeRepositoryImpl{myDB: db}
 }
 
-func (r *OperationCodeRepositoryImpl) GetAllOperationCode(filterCondition []utils.FilterCondition, pages pagination.Pagination) (pagination.Pagination, error) {
+func (r *OperationCodeRepositoryImpl) GetAllOperationCode(tx *gorm.DB,filterCondition []utils.FilterCondition, pages pagination.Pagination) (pagination.Pagination, *exceptionsss_test.BaseErrorResponse) {
 	entities := []masteroperationentities.OperationCode{}
 	//define base model
-	baseModelQuery := r.myDB.Model(&entities)
+	baseModelQuery := tx.Model(&entities)
 	//apply where query
 	whereQuery := utils.ApplyFilter(baseModelQuery, filterCondition)
 	//apply pagination and execute
 	rows, err := baseModelQuery.Scopes(pagination.Paginate(&entities, &pages, whereQuery)).Scan(&entities).Rows()
 
 	if len(entities) == 0 {
-		return pages, gorm.ErrRecordNotFound
+		return pages, &exceptionsss_test.BaseErrorResponse{
+			StatusCode: http.StatusNotFound,
+			Err:        err,
+		}
 	}
 
 	if err != nil {
-		return pages, err
+		return pages, &exceptionsss_test.BaseErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Err:        err,
+		}
 	}
 
 	defer rows.Close()
@@ -43,20 +52,11 @@ func (r *OperationCodeRepositoryImpl) GetAllOperationCode(filterCondition []util
 	return pages, nil
 }
 
-func (r *OperationCodeRepositoryImpl) WithTrx(trxHandle *gorm.DB) masteroperationrepository.OperationCodeRepository {
-	if trxHandle == nil {
-		log.Println("Transaction Database Not Found!")
-		return r
-	}
-	r.myDB = trxHandle
-	return r
-}
-
-func (r *OperationCodeRepositoryImpl) GetOperationCodeById(Id int32) (masteroperationpayloads.OperationCodeResponse, error) {
+func (r *OperationCodeRepositoryImpl) GetOperationCodeById(tx *gorm.DB, Id int) (masteroperationpayloads.OperationCodeResponse, *exceptionsss_test.BaseErrorResponse) {
 	entities := masteroperationentities.OperationCode{}
 	response := masteroperationpayloads.OperationCodeResponse{}
 
-	rows, err := r.myDB.Model(&entities).
+	rows, err := tx.Model(&entities).
 		Where(masteroperationentities.OperationCode{
 			OperationId: Id,
 		}).
@@ -64,10 +64,19 @@ func (r *OperationCodeRepositoryImpl) GetOperationCodeById(Id int32) (masteroper
 		Rows()
 
 	if err != nil {
-		return response, err
+		return response, &exceptionsss_test.BaseErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Err: err,
+		}
 	}
 
 	defer rows.Close()
 
 	return response, nil
+}
+
+func (r *OperationCodeRepositoryImpl) SaveOperationCode(tx *gorm.DB,req masteroperationpayloads.OperationEntriesRequest)(bool,*exceptionsss_test.BaseErrorResponse){
+	entities:masteroperationentities.OperationCode{
+		
+	}
 }
