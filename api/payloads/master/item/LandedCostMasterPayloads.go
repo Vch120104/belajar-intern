aaa2d877d88f32d5e617afe188b50f52e5f0b062
Nats46 <132544024+Nats46@users.noreply.github.com>
2024-06-07 13:16:11 +0700

    Landed cost master fix
    - add update landed cost master
    - fix get by id and get all landed cost master

--- api/payloads/master/item/LandedCostMasterPayloads.go
+++ api/payloads/master/item/LandedCostMasterPayloads.go
@@ -1,14 +1,13 @@
 package masteritempayloads
 
 type LandedCostMasterPayloads struct {
-	IsActive               bool    `json:"is_active" parent_entity:"mtr_landed_cost"`
-	LandedCostId           int     `json:"landed_cost_id" parent_entity:"mtr_landed_cost"`
-	CompanyId              int     `json:"company_id" parent_entity:"mtr_landed_cost"`
-	SupplierId             int     `json:"supplier_id" parent_entity:"mtr_landed_cost"`
-	ShippingMethodId       int     `json:"shipping_method_id" parent_id:"mtr_landed_cost"`
-	LandedCostTypeId       int     `json:"landed_cost_type_id" parent_entity:"mtr_landed_cost"`
-	LandedCostDescription  string  `json:"landed_cost_description" parent_entity:"mtr_landed_cost"`
-	LandedCostMasterFactor float64 `json:"landed_cost_master_factor" parent_entity:"mtr_landed_cost"`
+	IsActive         bool    `json:"is_active" parent_entity:"mtr_landed_cost"`
+	LandedCostId     int     `json:"landed_cost_id" parent_entity:"mtr_landed_cost"`
+	CompanyId        int     `json:"company_id" parent_entity:"mtr_landed_cost"`
+	SupplierId       int     `json:"supplier_id" parent_entity:"mtr_landed_cost"`
+	ShippingMethodId int     `json:"shipping_method_id" parent_entity:"mtr_landed_cost"`
+	LandedCostTypeId int     `json:"landed_cost_type_id" parent_entity:"mtr_landed_cost"`
+	LandedCostFactor float64 `json:"landed_cost_factor" parent_entity:"mtr_landed_cost"`
 }
 
 type ShippingMethodResponse struct {
@@ -19,5 +18,19 @@ type ShippingMethodResponse struct {
 type LandedCostTypeResponse struct {
 	LandedCostTypeId          int    `json:"landed_cost_type_id"`
 	LandedCostTypeCode        string `json:"landed_cost_type_code"`
-	LandedCostTypeDescription string `json:"landed_cost_type_description"`
+	LandedCostTypeDescription string `json:"landed_cost_type_name"`
+}
+
+type LandedCostMasterRequest struct {
+	IsActive         bool    `json:"is_acctive"`
+	CompanyId        int     `json:"company_id"`
+	SupplierId       int     `json:"supplier_id"`
+	ShippingMethodId int     `json:"shipping_method_id"`
+	LandedCostTypeId int     `json:"landed_cost_type_id"`
+	LandedCostId     int     `json:"landed_cost_id"`
+	LandedCostFactor float64 `json:"landed_cost_factor"`
+}
+
+type LandedCostMasterUpdateRequest struct {
+	LandedCostfactor float64 `json:"landed_cost_factor"`
 }
