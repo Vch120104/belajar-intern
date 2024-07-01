05ee1d0b8287744858487cbc043de0295f00ce4c
Ahmad Ridwan <ridwanrezani@gmail.com>
2024-07-01 16:56:43 +0700

    fixed(api): drop redis service in item/detail get all & by id

--- api/services/master/item/services-item-impl/ItemServiceImpl.go
+++ api/services/master/item/services-item-impl/ItemServiceImpl.go
@@ -8,18 +8,11 @@ import (
 	masteritemrepository "after-sales/api/repositories/master/item"
 	masteritemservice "after-sales/api/services/master/item"
 	"after-sales/api/utils"
-	"context"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"strings"
-	"time"
 
 	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
-
-const cacheExpiration = time.Minute * 30 // cache expiration time
 
 type ItemServiceImpl struct {
 	itemRepo    masteritemrepository.ItemRepository
@@ -33,47 +26,6 @@ func StartItemService(itemRepo masteritemrepository.ItemRepository, db *gorm.DB,
 		DB:          db,
 		RedisClient: redisClient,
 	}
-}
-
-// Function to generate cache key for GetAll
-func generateCacheKey(prefix string, filterCondition []utils.FilterCondition, pagination pagination.Pagination) string {
-
-	filterBytes, _ := json.Marshal(filterCondition)
-
-	pageStr := fmt.Sprintf("page=%d&size=%d", pagination.Page, pagination.Limit)
-
-	key := fmt.Sprintf("%s:%s:%s", prefix, filterBytes, pageStr)
-
-	return key
-
-	// // pakai ini kalau ingin di hash key
-	// hasher := sha1.New()
-	// hasher.Write([]byte(key))
-	// sha := hex.EncodeToString(hasher.Sum(nil))
-	// return sha
-}
-
-// Function to generate cache key for GetById
-func generateCacheKeyId(prefix string, params ...interface{}) string {
-
-	var paramStrs []string
-	for _, param := range params {
-		switch v := param.(type) {
-		case int:
-			paramStrs = append(paramStrs, fmt.Sprintf("%d", v))
-		case string:
-			paramStrs = append(paramStrs, v)
-		case []utils.FilterCondition:
-			filterBytes, _ := json.Marshal(v)
-			paramStrs = append(paramStrs, string(filterBytes))
-		case pagination.Pagination:
-			paramStrs = append(paramStrs, fmt.Sprintf("page=%d&size=%d", v.Page, v.Limit))
-		}
-	}
-
-	key := prefix + ":" + strings.Join(paramStrs, ":")
-
-	return key
 }
 
 // GetUomDropDown implements masteritemservice.ItemService.
@@ -99,49 +51,15 @@ func (s *ItemServiceImpl) GetUomTypeDropDown() ([]masteritempayloads.UomTypeDrop
 }
 
 func (s *ItemServiceImpl) GetAllItem(filterCondition []utils.FilterCondition, pages pagination.Pagination) ([]map[string]interface{}, int, int, *exceptions.BaseErrorResponse) {
-	ctx := context.Background()
-	cacheKey := generateCacheKey("item_master", filterCondition, pages)
 
-	cachedData, err := s.RedisClient.Get(ctx, cacheKey).Result()
-	if err == redis.Nil {
-		fmt.Println("Cache miss, querying database...")
-
-		tx := s.DB.Begin()
-		defer helper.CommitOrRollback(tx)
-		results, totalPages, totalRows, repoErr := s.itemRepo.GetAllItem(tx, filterCondition, pages)
-		if repoErr != nil {
-			return results, totalPages, totalRows, repoErr
-		}
-
-		cacheData, marshalErr := json.Marshal(results)
-		if marshalErr != nil {
-			fmt.Println("Failed to marshal results for caching:", marshalErr)
-		} else {
-			setErr := s.RedisClient.Set(ctx, cacheKey, cacheData, cacheExpiration).Err()
-			if setErr != nil {
-				fmt.Println("Failed to set cache:", setErr)
-			}
-		}
-
-		return results, totalPages, totalRows, nil
-	} else if err != nil {
-		return nil, 0, 0, &exceptions.BaseErrorResponse{
-			StatusCode: http.StatusInternalServerError,
-			Err:        err,
-		}
-	} else {
-		fmt.Println("Cache hit, returning cached data...")
-		var mapResponses []map[string]interface{}
-		if err := json.Unmarshal([]byte(cachedData), &mapResponses); err != nil {
-			return nil, 0, 0, &exceptions.BaseErrorResponse{
-				StatusCode: http.StatusInternalServerError,
-				Err:        err,
-			}
-		}
-
-		paginatedData, totalPages, totalRows := pagination.NewDataFramePaginate(mapResponses, &pages)
-		return paginatedData, totalPages, totalRows, nil
+	tx := s.DB.Begin()
+	defer helper.CommitOrRollback(tx)
+	results, totalPages, totalRows, repoErr := s.itemRepo.GetAllItem(tx, filterCondition, pages)
+	if repoErr != nil {
+		return results, totalPages, totalRows, repoErr
 	}
+	return results, totalPages, totalRows, nil
+
 }
 
 func (s *ItemServiceImpl) GetAllItemLookup(filter []utils.FilterCondition) (any, *exceptions.BaseErrorResponse) {
@@ -175,12 +93,10 @@ func (s *ItemServiceImpl) GetItemWithMultiId(MultiIds []string) ([]masteritempay
 }
 
 func (s *ItemServiceImpl) GetItemCode(code string) (masteritempayloads.ItemResponse, *exceptions.BaseErrorResponse) {
-	// Melakukan URL encoding pada parameter code
-	// encodedCode := url.PathEscape(code)
 
 	tx := s.DB.Begin()
 	defer helper.CommitOrRollback(tx)
-	results, err := s.itemRepo.GetItemCode(tx, code) // Menggunakan kode yang telah diencode
+	results, err := s.itemRepo.GetItemCode(tx, code)
 	if err != nil {
 		return results, err
 	}
@@ -224,89 +140,25 @@ func (s *ItemServiceImpl) ChangeStatusItem(Id int) (bool, *exceptions.BaseErrorR
 }
 
 func (s *ItemServiceImpl) GetAllItemDetail(filterCondition []utils.FilterCondition, pages pagination.Pagination) ([]map[string]interface{}, int, int, *exceptions.BaseErrorResponse) {
-	ctx := context.Background()
-	cacheKey := generateCacheKey("item_detail_master", filterCondition, pages)
-
-	cachedData, err := s.RedisClient.Get(ctx, cacheKey).Result()
-	if err == redis.Nil {
-		fmt.Println("Cache miss, querying database...")
-		tx := s.DB.Begin()
-		defer helper.CommitOrRollback(tx)
-		results, totalPages, totalRows, repoErr := s.itemRepo.GetAllItemDetail(tx, filterCondition, pages)
-		if repoErr != nil {
-			return results, totalPages, totalRows, repoErr
-		}
-
-		cacheData, marshalErr := json.Marshal(results)
-		if marshalErr != nil {
-			fmt.Println("Failed to marshal results for caching:", marshalErr)
-		} else {
-			setErr := s.RedisClient.Set(ctx, cacheKey, cacheData, cacheExpiration).Err()
-			if setErr != nil {
-				fmt.Println("Failed to set cache:", setErr)
-			}
-		}
-
-		return results, totalPages, totalRows, nil
-	} else if err != nil {
-		return nil, 0, 0, &exceptions.BaseErrorResponse{
-			StatusCode: http.StatusInternalServerError,
-			Err:        err,
-		}
-	} else {
-		fmt.Println("Cache hit, returning cached data...")
-		var mapResponses []map[string]interface{}
-		if err := json.Unmarshal([]byte(cachedData), &mapResponses); err != nil {
-			return nil, 0, 0, &exceptions.BaseErrorResponse{
-				StatusCode: http.StatusInternalServerError,
-				Err:        err,
-			}
-		}
-
-		paginatedData, totalPages, totalRows := pagination.NewDataFramePaginate(mapResponses, &pages)
-		return paginatedData, totalPages, totalRows, nil
-	}
-}
-
-func (s *ItemServiceImpl) GetItemDetailById(itemID, itemDetailID int) (masteritempayloads.ItemDetailRequest, *exceptions.BaseErrorResponse) {
-	ctx := context.Background()
-	cacheKey := generateCacheKeyId("item_detail", itemID, itemDetailID)
-
-	cachedData, err := s.RedisClient.Get(ctx, cacheKey).Result()
-	if err == nil {
-
-		var result masteritempayloads.ItemDetailRequest
-		if unmarshalErr := json.Unmarshal([]byte(cachedData), &result); unmarshalErr != nil {
-			return masteritempayloads.ItemDetailRequest{}, &exceptions.BaseErrorResponse{
-				StatusCode: http.StatusInternalServerError,
-				Err:        unmarshalErr,
-			}
-		}
-		return result, nil
-	} else if err != redis.Nil {
-
-		return masteritempayloads.ItemDetailRequest{}, &exceptions.BaseErrorResponse{
-			StatusCode: http.StatusInternalServerError,
-			Err:        err,
-		}
-	}
 
 	tx := s.DB.Begin()
 	defer helper.CommitOrRollback(tx)
-	result, repoErr := s.itemRepo.GetItemDetailById(tx, itemID, itemDetailID)
+	results, totalPages, totalRows, repoErr := s.itemRepo.GetAllItemDetail(tx, filterCondition, pages)
 	if repoErr != nil {
-		errorResponse := &exceptions.BaseErrorResponse{Message: repoErr.Message}
-		return masteritempayloads.ItemDetailRequest{}, errorResponse
+		return results, totalPages, totalRows, repoErr
 	}
 
-	cacheData, marshalErr := json.Marshal(result)
-	if marshalErr != nil {
-		fmt.Println("Failed to marshal results for caching:", marshalErr)
-	} else {
-		setErr := s.RedisClient.Set(ctx, cacheKey, cacheData, cacheExpiration).Err()
-		if setErr != nil {
-			fmt.Println("Failed to set cache:", setErr)
-		}
+	return results, totalPages, totalRows, nil
+
+}
+
+func (s *ItemServiceImpl) GetItemDetailById(itemID, itemDetailID int) (masteritempayloads.ItemDetailRequest, *exceptions.BaseErrorResponse) {
+
+	tx := s.DB.Begin()
+	defer helper.CommitOrRollback(tx)
+	result, err := s.itemRepo.GetItemDetailById(tx, itemID, itemDetailID)
+	if err != nil {
+		return result, err
 	}
 
 	return result, nil
