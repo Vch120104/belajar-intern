fd905f2562966fbcdf1ce1d9896a4db1623c4d0a
Nats46 <132544024+Nats46@users.noreply.github.com>
2024-07-04 15:39:19 +0700

    fix item substitute
    fix get item substitute

--- api/services/master/item/ItemSubstituteService.go
+++ api/services/master/item/ItemSubstituteService.go
@@ -4,16 +4,18 @@ import (
 	exceptions "after-sales/api/exceptions"
 	masteritempayloads "after-sales/api/payloads/master/item"
 	"after-sales/api/payloads/pagination"
+	"after-sales/api/utils"
+	"time"
 )
 
 type ItemSubstituteService interface {
-	GetByIdItemSubstitute(id int) (map[string]interface{}, *exceptions.BaseErrorResponse)
+	GetAllItemSubstitute(filterCondition []utils.FilterCondition, pages pagination.Pagination, from time.Time, to time.Time) (pagination.Pagination, *exceptions.BaseErrorResponse)
+	GetByIdItemSubstitute(id int) (masteritempayloads.ItemSubstitutePayloads, *exceptions.BaseErrorResponse)
 	GetAllItemSubstituteDetail(pagination.Pagination, int) (pagination.Pagination, *exceptions.BaseErrorResponse)
 	GetByIdItemSubstituteDetail(id int) (masteritempayloads.ItemSubstituteDetailGetPayloads, *exceptions.BaseErrorResponse)
 	SaveItemSubstitute(req masteritempayloads.ItemSubstitutePostPayloads) (bool, *exceptions.BaseErrorResponse)
 	SaveItemSubstituteDetail(req masteritempayloads.ItemSubstituteDetailPostPayloads, id int) (bool, *exceptions.BaseErrorResponse)
-	ChangeStatusItemOperation(id int) (bool, *exceptions.BaseErrorResponse)
+	ChangeStatusItemSubstitute(id int) (bool, *exceptions.BaseErrorResponse)
 	DeactivateItemSubstituteDetail(id string) (bool, *exceptions.BaseErrorResponse)
 	ActivateItemSubstituteDetail(id string) (bool, *exceptions.BaseErrorResponse)
-	GetAllItemSubstitute(filterCondition map[string]string, pages pagination.Pagination) ([]map[string]interface{},int,int, *exceptions.BaseErrorResponse)
 }
