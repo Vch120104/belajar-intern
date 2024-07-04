b8f208a78a1b68d16fa9ca526d2a29d1b464ff86
kenthken <kenth6880@gmail.com>
2024-07-04 09:42:30 +0700

    Update service for new function

--- api/services/master/item/services-item-impl/PriceListServiceImpl.go
+++ api/services/master/item/services-item-impl/PriceListServiceImpl.go
@@ -83,42 +83,46 @@ func (s *PriceListServiceImpl) ChangeStatusPriceList(Id int) (bool, *exceptions.
 	return result, nil
 }
 
-func (s *PriceListServiceImpl) GetAllPriceListNew (filterCondition []utils.FilterCondition, pages pagination.Pagination)([]map[string]interface{},int,int, *exceptions.BaseErrorResponse){
+func (s *PriceListServiceImpl) GetAllPriceListNew(filterCondition []utils.FilterCondition, pages pagination.Pagination) ([]map[string]interface{}, int, int, *exceptions.BaseErrorResponse) {
 	tx := s.DB.Begin()
-	defer helper.CommitOrRollback(tx)
-	result,total_page,total_rows,err := s.priceListRepo.GetAllPriceListNew(tx,filterCondition,pages)
-	if err != nil{
-		return nil,0,0,err
+	result, total_page, total_rows, err := s.priceListRepo.GetAllPriceListNew(tx, filterCondition, pages)
+	if err != nil {
+		return nil, 0, 0, err
 	}
-	return result,total_page,total_rows,nil
+	defer helper.CommitOrRollback(tx, err)
+
+	return result, total_page, total_rows, nil
 }
 
-func (s *PriceListServiceImpl) DeactivatePriceList(id string)(bool,*exceptions.BaseErrorResponse){
+func (s *PriceListServiceImpl) DeactivatePriceList(id string) (bool, *exceptions.BaseErrorResponse) {
 	tx := s.DB.Begin()
-	defer helper.CommitOrRollback(tx)
-	result,err := s.priceListRepo.DeactivatePriceList(tx,id)
-	if err != nil{
-		return false,err
+	result, err := s.priceListRepo.DeactivatePriceList(tx, id)
+	if err != nil {
+		return false, err
 	}
-	return result,nil
+	defer helper.CommitOrRollback(tx, err)
+
+	return result, nil
 }
 
-func (s *PriceListServiceImpl) ActivatePriceList(id string)(bool,*exceptions.BaseErrorResponse){
-	tx :=s.DB.Begin()
-	defer helper.CommitOrRollback(tx)
-	result,err := s.priceListRepo.ActivatePriceList(tx,id)
-	if err != nil{
-		return false,err
+func (s *PriceListServiceImpl) ActivatePriceList(id string) (bool, *exceptions.BaseErrorResponse) {
+	tx := s.DB.Begin()
+	result, err := s.priceListRepo.ActivatePriceList(tx, id)
+	if err != nil {
+		return false, err
 	}
-	return result,nil
+	defer helper.CommitOrRollback(tx, err)
+
+	return result, nil
 }
 
-func (s *PriceListServiceImpl) DeletePriceList(id string)(bool,*exceptions.BaseErrorResponse){
+func (s *PriceListServiceImpl) DeletePriceList(id string) (bool, *exceptions.BaseErrorResponse) {
 	tx := s.DB.Begin()
-	defer helper.CommitOrRollback(tx)
-	result,err := s.priceListRepo.DeletePriceList(tx,id)
-	if err != nil{
-		return false,err
+	result, err := s.priceListRepo.DeletePriceList(tx, id)
+	if err != nil {
+		return false, err
 	}
-	return result,nil
+	defer helper.CommitOrRollback(tx, err)
+
+	return result, nil
 }
