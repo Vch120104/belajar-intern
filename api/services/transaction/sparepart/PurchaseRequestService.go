653bb8f434d1abd67999b12ea55b474c0894e190
devinluize <71080055+devinluize@users.noreply.github.com>
2024-07-31 17:06:42 +0700

    Create PurchaseRequestService.go

---
+++ api/services/transaction/sparepart/PurchaseRequestService.go
@@ -0,0 +1,18 @@
+package transactionsparepartservice
+
+import (
+	transactionsparepartentities "after-sales/api/entities/transaction/sparepart"
+	"after-sales/api/exceptions"
+	"after-sales/api/payloads/pagination"
+	transactionsparepartpayloads "after-sales/api/payloads/transaction/sparepart"
+	"after-sales/api/utils"
+)
+
+type PurchaseRequestService interface {
+	//sddas
+	GetAllPurchaseRequest(filterCondition []utils.FilterCondition, pages pagination.Pagination, Dateparams map[string]string) (pagination.Pagination, *exceptions.BaseErrorResponse)
+	GetByIdPurchaseRequest(id int) (transactionsparepartpayloads.PurchaseRequestGetByIdNormalizeResponses, *exceptions.BaseErrorResponse)
+	GetAllPurchaseRequestDetail(filterCondition []utils.FilterCondition, pages pagination.Pagination) (pagination.Pagination, *exceptions.BaseErrorResponse)
+	GetByIdPurchaseRequestDetail(id int) (transactionsparepartpayloads.PurchaseRequestDetailResponsesPayloads, *exceptions.BaseErrorResponse)
+	PurchaseRequestSaveNewHeader(transactionsparepartpayloads.PurchaseRequestHeaderSaveRequest) (transactionsparepartentities.PurchaseRequestEntities, *exceptions.BaseErrorResponse)
+}
