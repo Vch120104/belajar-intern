9a54bd27824755f8e14624c08326c32f38117154
stephensfx <132635897+stephensfx@users.noreply.github.com>
2024-08-15 09:50:56 +0700

    remove unused struct

--- api/payloads/transaction/sparepart/SupplySlipDetailPayloads.go
+++ api/payloads/transaction/sparepart/SupplySlipDetailPayloads.go
@@ -29,24 +29,6 @@ type SupplySlipDetailByHeaderIdResponse struct {
 	QuantityPR               float32 `json:"quantity_pr"`
 }
 
-type SupplySlipDetailRequest struct {
-	SupplySystemNumbers               int32   `json:"supply_system_number"`
-	SupplySystemLineNumber            int32   `json:"supply_system_line_number"`
-	LocationId                        int32   `json:"location_id"`
-	WorkOrderItemId                   int32   `json:"work_order_item_id"`
-	UnitOutMeasurementId              int32   `json:"unit_out_measurement_id"`
-	QuantitySupply                    float64 `json:"quantity_supply"`
-	QuantityReturn                    float64 `json:"quantity_return"`
-	QuantityDemand                    float64 `json:"quantity_demand"`
-	CostOfGoodsSold                   float64 `json:"cost_of_goods_sold"`
-	PurchaseRequestSystemNumber       int32   `json:"purchase_request_system_number"`
-	PurchaseRequestSystemNumberDetail int32   `json:"purchase_request_system_number_detail"`
-	WorkOrderSystemNumber             int32   `json:"work_order_system_number"`
-	WorkOrderLineNumberId             int32   `json:"work_order_line_number_id"`
-	WarehouseGroupId                  int32   `json:"warehouse_group_id"`
-	WarehouseId                       int32   `json:"warehouse_id"`
-	QuantityTotal                     int32   `json:"quantity_total"`
-}
 
 type SupplySlipDetailsResponse struct {
 	Page       int                                  `json:"page"`
--- api/payloads/transaction/sparepart/SupplySlipPayloads.go
+++ api/payloads/transaction/sparepart/SupplySlipPayloads.go
@@ -1,47 +1,5 @@
 package transactionsparepartpayloads
 
-import (
-	"time"
-)
-
-// type SupplySlipResponse struct {
-// 	IsActive              bool      `json:"is_active"`
-// 	SupplySystemNumber    int32     `json:"supply_system_number"`
-// 	SupplyDocumentNumber  string    `json:"supply_document_number"`
-// 	SupplyStatusId        int32     `json:"supply_status_id"`
-// 	SupplyDate            time.Time `json:"supply_date"`
-// 	SupplyTypeId          int32     `json:"supply_type_id"`
-// 	CompanyId             int32     `json:"company_id"`
-// 	WorkOrderSystemNumber int32     `json:"work_order_system_number"`
-// 	ProfitCenterId        int32     `json:"profit_center_id"`
-// 	BrandId               int32     `json:"brand_id"`
-// 	ModelId               int32     `json:"model_id"`
-// 	VariantId             int32     `json:"variant_id"`
-// 	VehicleId             int32     `json:"vehicle_id"`
-// 	CustomerId            int32     `json:"customer_id"`
-// 	TechnicianId          int32     `json:"technician_id "`
-// 	CampaignId            int32     `json:"campaign_id"`
-// 	Remark                string    `json:"remark"`
-// }
-
-type SupplySlipRequest struct {
-	SupplyDocumentNumber  string    `json:"supply_document_number"`
-	SupplyStatusId        int32     `json:"supply_status_id"`
-	SupplyDate            time.Time `json:"supply_date"`
-	SupplyTypeId          int32     `json:"supply_type_id"`
-	CompanyId             int32     `json:"company_id"`
-	WorkOrderSystemNumber int32     `json:"work_order_system_number"`
-	ProfitCenterId        int32     `json:"profit_center_id"`
-	BrandId               int32     `json:"brand_id"`
-	ModelId               int32     `json:"model_id"`
-	VariantId             int32     `json:"variant_id"`
-	VehicleId             int32     `json:"vehicle_id"`
-	CustomerId            int32     `json:"customer_id"`
-	TechnicianId          int32     `json:"technician_id "`
-	CampaignId            int32     `json:"campaign_id"`
-	Remark                string    `json:"remark"`
-}
-
 type SupplySlipSearchResponse struct {
 	SupplySystemNumber      int    `json:"supply_system_number" parent_entity:"trx_supply_slip" main_table:"trx_supply_slip"`
 	SupplyDocumentNumber    string `json:"supply_document_number" parent_entity:"trx_supply_slip"`
@@ -114,13 +72,6 @@ type VariantResponse struct {
 	VariantCode        string `json:"variant_code"`
 	VariantDescription string `json:"variant_description"`
 	ProductionYear     string `json:"production_year"`
-}
-
-type BrandDocResponse struct {
-	BrandId           int    `json:"brand_id"`
-	BrandCode         string `json:"brand_code"`
-	BrandName         string `json:"brand_name"`
-	BrandAbbreviation string `json:"brand_abbreveation"`
 }
 
 type SubmitSupplySlipResponse struct {
