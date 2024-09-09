dd701a94093b346d49909b40165e4b4e03e13fbe
Nats46 <132544024+Nats46@users.noreply.github.com>
2024-09-09 13:44:18 +0700

    fix big int on entity

--- api/entities/master/CampaignMasterDetailEntity.go
+++ api/entities/master/CampaignMasterDetailEntity.go
@@ -3,16 +3,17 @@ package masterentities
 var CreateCampaignMasterDetailTable = "mtr_campaign_master_detail"
 
 type CampaignMasterDetail struct {
-	IsActive         bool    `gorm:"column:is_active;not null;default:true" json:"is_active"`
-	CampaignDetailId int     `gorm:"column:campaign_detail_id;primaryKey;autoIncrement;size:30" json:"campaign_detail_id"`
-	CampaignId       int     `gorm:"column:campaign_id;not null;index:idx_campaign_operationitemcode;size:30" json:"campaign_id"`
-	LineTypeId       int     `gorm:"column:line_type_id;not null;size:30" json:"line_type_id"`
-	Quantity         float64 `gorm:"column:quantity" json:"quantity"`
-	ItemOperationId  int     `gorm:"column:item_operation_id;index:idx_campaign_operationitemcode;size:30" json:"item_operation_id"`
-	ShareBillTo      string  `gorm:"column:share_bill_to;size:10" json:"share_bill_to"`
-	DiscountPercent  float64 `gorm:"column:discount_percent" json:"discount_percent"`
-	SharePercent     float64 `gorm:"column:share_percent" json:"share_percent"`
-	Price            float64 `gorm:"column:price" json:"price"`
+	IsActive            bool           `gorm:"column:is_active;not null;default:true" json:"is_active"`
+	CampaignDetailId    int            `gorm:"column:campaign_detail_id;primaryKey;autoIncrement;size:30" json:"campaign_detail_id"`
+	CampaignId          int            `gorm:"column:campaign_id;not null;index:idx_campaign_operationitemcode;size:30" json:"campaign_id"`
+	LineTypeId          int            `gorm:"column:line_type_id;not null;size:30" json:"line_type_id"`
+	Quantity            float64        `gorm:"column:quantity" json:"quantity"`
+	ItemOperationId     int            `gorm:"column:item_operation_id;index:idx_campaign_operationitemcode;size:30" json:"item_operation_id"`
+	ShareBillTo         string         `gorm:"column:share_bill_to;size:10" json:"share_bill_to"`
+	DiscountPercent     float64        `gorm:"column:discount_percent" json:"discount_percent"`
+	SharePercent        float64        `gorm:"column:share_percent" json:"share_percent"`
+	Price               float64        `gorm:"column:price" json:"price"`
+	ItemOperationEntity *ItemOperation `gorm:"foreignKey:ItemOperationId;references:ItemOperationId"`
 }
 
 func (*CampaignMasterDetail) TableName() string {
--- api/entities/master/ItemOperationEntity.go
+++ api/entities/master/ItemOperationEntity.go
@@ -3,11 +3,12 @@ package masterentities
 const ItemOperationTableName = "mtr_item_operation"
 
 type ItemOperation struct {
-	ItemOperationId         int `gorm:"column:item_operation_id;not null;primaryKey" json:"item_operation_id"`
-	ItemId                  int `gorm:"column:item_id;not null;size:30;uniqueIndex:item_cycle;default:0" json:"item_id"`
-	OperationModelMappingId int `gorm:"column:operation_model_mapping_id;not null;uniqueIndex:item_cycle;default:0;size:30" json:"operation_model_mapping_id"`
-	LineTypeId              int `gorm:"column:line_type_id;not null" json:"line_type_id"`
-	PackageMasterDetail     *PackageMasterDetail
+	ItemOperationId         int                   `gorm:"column:item_operation_id;not null;primaryKey;size:30" json:"item_operation_id"`
+	ItemId                  int                   `gorm:"column:item_id;not null;size:30;uniqueIndex:item_cycle;default:0" json:"item_id"`
+	OperationModelMappingId int                   `gorm:"column:operation_model_mapping_id;not null;uniqueIndex:item_cycle;default:0;size:30" json:"operation_model_mapping_id"`
+	LineTypeId              int                   `gorm:"column:line_type_id;not null" json:"line_type_id"`
+	PackageMasterDetail     *PackageMasterDetail  `gorm:"foreignKey:ItemOperationId;references:ItemOperationId"`
+	CampaignMasterDetail    *CampaignMasterDetail `gorm:"foreignKey:ItemOperationId;references:ItemOperationId"`
 }
 
 func (*ItemOperation) TableName() string {
