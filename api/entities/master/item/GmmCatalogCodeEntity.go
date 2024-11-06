86c657ce70a4d0157fa26c7d39a75dc1e802673e
mario sebastian <mariosebastian612@gmail.com>
2024-11-06 11:31:43 +0700

    add is active column to gmm catalog

--- api/entities/master/item/GmmCatalogCodeEntity.go
+++ api/entities/master/item/GmmCatalogCodeEntity.go
@@ -3,6 +3,7 @@ package masteritementities
 var CreateGmmCatalogCodeTable = "mtr_gmm_catalog_code"
 
 type GmmCatalogCode struct {
+	IsActive              bool   `gorm:"column:is_active;not null;default:true"`
 	GmmCatalogId          int    `gorm:"column:gmm_catalog_id;size:30;primaryKey" json:"gmm_catalog_id"`
 	GmmCatalogCode        string `gorm:"column:gmm_catalog_code;unique;not null" json:"gmm_catalog_code"`
 	GmmCatalogDescription string `gorm:"column:gmm_catalog_description;not null" json:"gmm_catalog_description"`
