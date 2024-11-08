7a176023cfef7c9faccefab147d015261a720855
mario sebastian <mariosebastian612@gmail.com>
2024-11-08 10:33:31 +0700

    rename gorm tag

--- api/entities/master/item/MarkUpEntity.go
+++ api/entities/master/item/MarkUpEntity.go
@@ -5,7 +5,7 @@ var CreateMarkupMasterTable = "mtr_markup_master"
 type MarkupMaster struct {
 	IsActive          bool   `gorm:"column:is_active;not null;default:true"        json:"is_active"`
 	MarkupMasterId    int    `gorm:"column:markup_master_id;size:30;not null;primaryKey"        json:"markup_master_id"`
-	MarkupCode        string `gorm:"column:markup_code;unique;size:10;not null"        json:"markup_master_code"`
+	MarkupCode        string `gorm:"column:markup_code;unique;size:10;not null"        json:"markup_code"`
 	MarkupDescription string `gorm:"column:markup_description;size:20;not null"        json:"markup_description"`
 }
 
