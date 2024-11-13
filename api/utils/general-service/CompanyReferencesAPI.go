814a0f398a3f77662a5c0bf18178a847cf21bcc8
devinluize <devin.luize12@gmail.com>
2024-11-13 15:08:38 +0700

    refactor cross service to utils cross service

---
+++ api/utils/general-service/CompanyReferencesAPI.go
@@ -0,0 +1,44 @@
+package generalserviceapiutils
+
+import (
+	"after-sales/api/config"
+	"after-sales/api/exceptions"
+	"after-sales/api/utils"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type CompanyReferenceBetByIdResponse struct {
+	CurrencyId                int         `json:"currency_id"`
+	CoaGroupId                int         `json:"coa_group_id"`
+	OperationDiscountOuterKpp json.Number `json:"operation_discount_outer_kpp"`
+	MarginOuterKpp            json.Number `json:"margin_outer_kpp"`
+	AdjustmentReasonId        int         `json:"adjustment_reason_id"`
+	LeadTimeUnitEtd           int         `json:"lead_time_unit_etd"`
+	BankAccReceiveCompanyId   int         `json:"bank_acc_receive_company_id"`
+	UnitWarehouseId           int         `json:"unit_warehouse_id"`
+	TimeDifference            int         `json:"time_difference"`
+	UseDms                    bool        `json:"use_dms"`
+	UseJpcb                   bool        `json:"use_jpcb"`
+	CheckMonthEnd             bool        `json:"check_month_end"`
+	IsDistributor             bool        `json:"is_distributor"`
+	WithVat                   bool        `json:"with_vat"`
+}
+
+func GetCompanyReferenceById(id int) (CompanyReferenceBetByIdResponse, *exceptions.BaseErrorResponse) {
+	CompanyReferenceBetByIdResponseData := CompanyReferenceBetByIdResponse{}
+
+	CompanyReferenceUrl := fmt.Sprintf("%scompany-reference/%s", config.EnvConfigs.GeneralServiceUrl, strconv.Itoa(id))
+	//errFetchCompany := utils.Get(CompanyReferenceUrl, &CompanyReferenceBetByIdResponseData, nil)
+	errFetchCompany := utils.CallAPI("GET", CompanyReferenceUrl, nil, &CompanyReferenceBetByIdResponseData)
+	if errFetchCompany != nil {
+		return CompanyReferenceBetByIdResponseData, &exceptions.BaseErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Err:        errFetchCompany,
+			Message:    errFetchCompany.Error(),
+		}
+	}
+	return CompanyReferenceBetByIdResponseData, nil
+}
