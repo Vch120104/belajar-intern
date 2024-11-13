bc79c28be4605968d7abd4f1d24e4ffdd44ca6eb
devinluize <devin.luize12@gmail.com>
2024-11-13 15:11:36 +0700

    delete unused payload

--- api/payloads/cross-service/general-service/ApprovalDocumentStatusPayloads.go
+++
@@ -1,8 +0,0 @@
-package generalservicepayloads
-
-//type ApprovalStatusResponses struct {
-//	IsActive                  bool   `json:"is_active"`
-//	ApprovalStatusId          int    `json:"approval_status_id"`
-//	ApprovalStatusCode        string `json:"approval_status_code"`
-//	ApprovalStatusDescription string `json:"approval_status_description"`
-//}
--- api/payloads/cross-service/general-service/DocumentStatusPayloads.go
+++
@@ -1,8 +0,0 @@
-package generalservicepayloads
-
-//type DocumentStatusPayloads struct {
-//	DocumentStatusCode        string `json:"document_status_code"`
-//	DocumentStatusDescription string `json:"document_status_description"`
-//	IsActive                  bool   `json:"is_active"`
-//	DocumentStatusId          int    `json:"document_status_id"`
-//}
