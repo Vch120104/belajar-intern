3989bbae1d3f6ef01fb8f825449d6a233c79dae8
mario sebastian <mariosebastian612@gmail.com>
2024-11-29 17:03:43 +0700

    remove unused api endpoint

--- api/utils/general-service/ItemGroupAPI.go
+++ api/utils/general-service/ItemGroupAPI.go
@@ -4,12 +4,8 @@ import (
 	"after-sales/api/config"
 	"after-sales/api/exceptions"
 	"after-sales/api/utils"
-	"errors"
-	"fmt"
 	"net/http"
-	"reflect"
 	"strconv"
-	"strings"
 )
 
 type ItemGroupResponse struct {
@@ -29,62 +25,4 @@ func GetItemGroupById(itemGroupId int) (ItemGroupResponse, *exceptions.BaseError
 		}
 	}
 	return ItemGroup, nil
-}
-
-type GetAllItemGroupParams struct {
-	Page          int    `json:"page"`
-	Limit         int    `json:"limit"`
-	IsActive      string `json:"is_active"`
-	ItemGroupCode string `json:"item_group_code"`
-	ItemGroupName string `json:"item_group_name"`
-	SortBy        string `json:"sort_by"`
-	SortOf        string `json:"sort_of"`
-}
-
-type GetAllItemGroupResponse struct {
-	StatusCode int                 `json:"status_code"`
-	Message    string              `json:"message"`
-	Data       []ItemGroupResponse `json:"data"`
-}
-
-func GetAllItemGroup(params GetAllItemGroupParams) (GetAllItemGroupResponse, *exceptions.BaseErrorResponse) {
-	var getItemGroup GetAllItemGroupResponse
-	if params.Limit == 0 {
-		params.Limit = 1000000
-	}
-
-	baseURL := config.EnvConfigs.GeneralServiceUrl + "item-groups"
-
-	queryParams := fmt.Sprintf("page=%d&limit=%d", params.Page, params.Limit)
-
-	v := reflect.ValueOf(params)
-	typeOfParams := v.Type()
-	for i := 0; i < v.NumField(); i++ {
-		value := v.Field(i).Interface()
-		if strVal, ok := value.(string); ok && strVal != "" {
-			key := typeOfParams.Field(i).Tag.Get("json")
-			value := strings.ReplaceAll(strVal, " ", "%20")
-			queryParams += "&" + key + "=" + value
-		}
-	}
-
-	url := baseURL + "?" + queryParams
-
-	err := utils.GetArray(url, nil, &getItemGroup)
-	if err != nil {
-		status := http.StatusBadGateway // Default to 502
-		message := "Failed to retrieve item group due to an external service error"
-
-		if errors.Is(err, utils.ErrServiceUnavailable) {
-			status = http.StatusServiceUnavailable
-			message = "item group service is temporarily unavailable"
-		}
-
-		return getItemGroup, &exceptions.BaseErrorResponse{
-			StatusCode: status,
-			Message:    message,
-			Err:        errors.New("error consuming external API while getting item group"),
-		}
-	}
-	return getItemGroup, nil
 }
