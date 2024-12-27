c8df7639c76482d3010b814d3c9e002532407355
Ahmad Ridwan <ridwanrezani@gmail.com>
2024-12-27 17:46:41 +0700

    update conflict campaign & package

--- api/entities/master/PackageMasterDetailEntity.go
+++ api/entities/master/PackageMasterDetailEntity.go
@@ -7,7 +7,6 @@ type PackageMasterDetail struct {
 	PackageDetailId            int                   `gorm:"column:package_detail_id;size:30;not null;primaryKey" json:"package_detail_id"`
 	PackageId                  int                   `gorm:"column:package_id;unique;size:30;not null" json:"package_id"`
 	LineTypeId                 int                   `gorm:"column:line_type_id;size:30;not null" json:"line_type_id"`
-	LineTypeCode               string                `gorm:"column:line_type_code;size:30;not null" json:"line_type_code"`
 	ItemOperationId            int                   `gorm:"column:item_operation_id;size:30;not null" json:"item_operation_id"`
 	FrtQuantity                float64               `gorm:"column:frt_quantity;not null" json:"frt_quantity"`
 	WorkorderTransactionTypeId int                   `gorm:"column:workorder_transaction_type_id;size:30;not null" json:"workorder_transaction_type_id"`
