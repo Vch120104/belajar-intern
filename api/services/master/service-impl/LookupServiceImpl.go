049b9d43c6de6b1cbd298d15ccded9de8c6c4ed7
mario sebastian <mariosebastian612@gmail.com>
2025-01-02 09:37:44 +0700

    add lookup LocationItem

--- api/services/master/service-impl/LookupServiceImpl.go
+++ api/services/master/service-impl/LookupServiceImpl.go
@@ -878,3 +878,36 @@ func (s *LookupServiceImpl) GetPartNumberItemImport(filterCondition []utils.Filt
 
 	return lookup, nil
 }
+
+func (s *LookupServiceImpl) LocationItem(filterCondition []utils.FilterCondition, pages pagination.Pagination) (pagination.Pagination, *exceptions.BaseErrorResponse) {
+	tx := s.DB.Begin()
+	var err *exceptions.BaseErrorResponse
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+			err = &exceptions.BaseErrorResponse{
+				StatusCode: http.StatusInternalServerError,
+				Err:        fmt.Errorf("panic recovered: %v", r),
+			}
+		} else if err != nil {
+			tx.Rollback()
+			logrus.Info("Transaction rollback due to error:", err)
+		} else {
+			if commitErr := tx.Commit().Error; commitErr != nil {
+				logrus.WithError(commitErr).Error("Transaction commit failed")
+				err = &exceptions.BaseErrorResponse{
+					StatusCode: http.StatusInternalServerError,
+					Err:        fmt.Errorf("failed to commit transaction: %w", commitErr),
+				}
+			}
+		}
+	}()
+
+	lookup, baseErr := s.LookupRepo.LocationItem(tx, filterCondition, pages)
+	if baseErr != nil {
+		return pages, baseErr
+	}
+
+	return lookup, nil
+}
