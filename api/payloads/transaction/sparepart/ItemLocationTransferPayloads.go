9fc71c494816f867e04bcf79b28c26108357ec90
Arya Yudha Saputra <100254907+AryaYS@users.noreply.github.com>
2025-01-24 09:44:59 +0700

    Fix Item Location Payloads

--- api/payloads/transaction/sparepart/ItemLocationTransferPayloads.go
+++ api/payloads/transaction/sparepart/ItemLocationTransferPayloads.go
@@ -57,13 +57,13 @@ type GetItemLocationTransferByIdResponse struct {
 }
 
 type InsertItemLocationTransferRequest struct {
-	CompanyId               int     `json:"company_id" validate:"required"`
-	TransferRequestById     *int    `json:"transfer_request_by_id"`
-	RequestFromWarehouseId  int     `json:"request_from_warehouse_id" validate:"required"`
-	RequestToWarehouseId    int     `json:"request_to_warehouse_id" validate:"required"`
-	Purpose                 *string `json:"purpose"`
-	TransferInSystemNumber  *int    `json:"transfer_in_system_number"`
-	TransferOutSystemNumber *int    `json:"transfer_out_system_number"`
+	CompanyId               int    `json:"company_id" validate:"required"`
+	TransferRequestById     *int   `json:"transfer_request_by_id"`
+	RequestFromWarehouseId  int    `json:"request_from_warehouse_id" validate:"required"`
+	RequestToWarehouseId    int    `json:"request_to_warehouse_id" validate:"required"`
+	Purpose                 string `json:"purpose"`
+	TransferInSystemNumber  *int   `json:"transfer_in_system_number"`
+	TransferOutSystemNumber *int   `json:"transfer_out_system_number"`
 }
 
 type UpdateItemLocationTransferRequest struct {
